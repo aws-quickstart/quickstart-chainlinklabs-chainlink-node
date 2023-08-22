626e49008f7db7f3d3dc8bec8d8f7e69f6831b53
Marcia Riefer Johnston <61847119+MarciaRieferJohnston@users.noreply.github.com>
2023-08-22 15:36:06 -0700

    Docs 2.1 conversion

--- scripts/create-api.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-printf "$1
-$2" > /home/ec2-user/.chainlink/.api
-
-echo
-echo -e "Created Chainlink node API file"
-echo
--- scripts/create-password.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-printf "$1" > /home/ec2-user/.chainlink/.password
-
-echo
-echo -e "Created keystore password file"
-echo
